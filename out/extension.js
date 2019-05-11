19cbbfc4c0fadc8074dff528202bf5583d683a2f
evgenii <ibanez.n04@gmail.com>
2019-05-11 21:20:08 +0300

    opens .liss file after running gpssh

--- out/extension.js
+++ out/extension.js
@@ -1,29 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "gpss" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.runGpssModel', () => {
-        // The code you place here will be executed every time your command is executed
-        let a = vscode.window.activeTextEditor;
-        var child = require('child_process').exec;
-        let fspath = "not found";
-        let path = "not found";
-        if (a != null) {
-            fspath = a.document.uri.fsPath;
-            path = fspath.substring(0, fspath.lastIndexOf("\\"));
+        let currentWindow = vscode.window.activeTextEditor;
+        var child = require('child_process').execSync;
+        const pathlib = require('path');
+        let fullFilePath;
+        let path;
+        if (currentWindow != null) {
+            fullFilePath = currentWindow.document.uri.fsPath;
+            path = pathlib.dirname(fullFilePath);
         }
-        path = path + "\\gpssh.exe" + " " + fspath;
-        let answer = child(path, (e, stdout, stderr) => {
+        let execPath = path + "\\gpssh.exe" + " " + fullFilePath;
+        child(execPath, (e, stdout, stderr) => {
             if (e instanceof Error) {
                 console.error(e);
                 throw e;
@@ -31,11 +22,15 @@ function activate(context) {
             console.log('stdout ', stdout);
             console.log('stderr ', stderr);
         });
+        let filename = pathlib.parse(fullFilePath).name;
+        var openPath = vscode.Uri.parse("file:///" + path + "\\" + filename + ".liss");
+        vscode.workspace.openTextDocument(openPath).then(doc => {
+            vscode.window.showTextDocument(doc);
+        });
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { print } from 'util';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gpss" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.runGpssModel', () => {
-		// The code you place here will be executed every time your command is executed
-		let a = vscode.window.activeTextEditor;
-		var child = require('child_process').exec;
-		let fspath = "not found";
-		let path = "not found";
-		if (a != null) {
-			fspath = a.document.uri.fsPath;
-			path = fspath.substring(0, fspath.lastIndexOf("\\"));
+		let currentWindow = vscode.window.activeTextEditor;
+		var child = require('child_process').execSync;
+		const pathlib = require('path');
+
+		let fullFilePath;
+		let path;
+		if (currentWindow != null) {
+			fullFilePath = currentWindow.document.uri.fsPath;
+			path = pathlib.dirname(fullFilePath);
+
 		}
-		path = path + "\\gpssh.exe" + " " + fspath;
-		let answer = child(path, (e: any, stdout: any, stderr: any) => {
+		let execPath = path + "\\gpssh.exe" + " " + fullFilePath;
+		child(execPath, (e: any, stdout: any, stderr: any) => {
 			if (e instanceof Error) {
 				console.error(e);
 				throw e;
@@ -32,10 +24,14 @@ export function activate(context: vscode.ExtensionContext) {
 			console.log('stdout ', stdout);
 			console.log('stderr ', stderr);
 		});
+
+		let filename = pathlib.parse(fullFilePath).name;
+		var openPath = vscode.Uri.parse("file:///" + path + "\\" + filename + ".liss");
+		vscode.workspace.openTextDocument(openPath).then(doc => {
+			vscode.window.showTextDocument(doc);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
 export function deactivate() { }
